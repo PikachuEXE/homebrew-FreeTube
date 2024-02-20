e33d107853ebb5a47df220ed04618b3e41ba3c49
PikachuEXE <git@pikachuexe.net>
2024-02-20 14:59:21 +0800

    * Use values from https://github.com/Homebrew/homebrew-cask/pull/167117

--- Casks/f/freetube.rb
+++ Casks/f/freetube.rb
@@ -1,7 +1,7 @@
 cask "freetube" do
   # Change this when release contains arm64 version
   arch arm: "x64", intel: "x64"
-  
+
   version "0.19.1"
   sha256  arm:   "4a8f77000e01a6123a3252f5217744d6e9836f78642c222081917e2318fa1c60",
           intel: "4a8f77000e01a6123a3252f5217744d6e9836f78642c222081917e2318fa1c60"
@@ -9,19 +9,28 @@ cask "freetube" do
   url "https://github.com/FreeTubeApp/FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac-#{arch}.dmg",
     verified: "https://github.com/FreeTubeApp/FreeTube/releases/download/"
   name "FreeTube"
-  desc "The Private YouTube Client"
-  homepage "https://freetubeapp.io"
+  desc "YouTube player focusing on privacy"
+  homepage "https://github.com/FreeTubeApp/FreeTube"
 
   livecheck do
     url :url
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    regex(/^v?(\d+(?:\.\d+)+)/i)
   end
 
-  depends_on macos: ">= :el_capitan"
+  depends_on macos: ">= :high_sierra"
 
   app "FreeTube.app"
 
+  uninstall quit: "io.freetubeapp.freetube"
+
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/io.freetubeapp.freetube.sfl*",
     "~/Library/Application Support/FreeTube",
+    "~/Library/Preferences/io.freetubeapp.freetube.plist",
+    "~/Library/Saved Application State/io.freetubeapp.freetube.savedState",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
